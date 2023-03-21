fc5a80532b4a48ae56f2529d47ce36427855509f
Maria Olbrich <olbrich15@gmail.com>
2023-03-21 20:02:30 +0100

    youtube video

--- main.js
+++
@@ -1,9 +0,0 @@
-const targetDiv = document.getElementById("third");
-const btn = document.getElementById("toggle");
-btn.onclick = function () {
-  if (targetDiv.style.display !== "none") {
-    targetDiv.style.display = "none";
-  } else {
-    targetDiv.style.display = "block";
-  }
-};
